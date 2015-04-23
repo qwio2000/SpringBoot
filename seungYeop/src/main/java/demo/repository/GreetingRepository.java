83a76d6421f585b71c6a403c540140a204bb1f88
qwio2000 <qwio2000@naver.com>
2015-04-23 09:24:53 +0900

    Spring Boot

--- seungYeop/src/main/java/demo/repository/GreetingRepository.java
+++ seungYeop/src/main/java/demo/repository/GreetingRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import demo.model.Friends;
 
 public interface GreetingRepository extends JpaRepository<Friends, Long> {
-	public Friends findOne(Long id);
+//	public Friends findOne(Long id);
 	public Friends findByUserId(String userId);
 }
